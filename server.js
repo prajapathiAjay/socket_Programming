f3dcd38d8484cc22e22f1045cc7ea3f2e92b67fb
prajapathiAjay <ajayworking6309@gmail.com>
2025-11-06 20:34:39 +0530

    established connection between client and server

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const io = new Server(server, { cors: { origin: "*", methods: ["GET", "POST"] } 
 
 
 io.on("connection", (socket) => {
-    console.log(`User connected: ${socket.id}`);
+    console.log(`User connected:`);
     socket.on("disconnect", () => {
         console.log(`User disconnected: ${socket.id}`);
     })
